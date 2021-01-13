7ae7ae19c4cadb91ad104c566a52d9a0b4432d32
jlzsss <jlz19821031@gmail.com>
2021-01-13 14:24:42 +0800

    Update diy-part1-lede.sh

--- diy-part1-lede.sh
+++ diy-part1-lede.sh
@@ -29,7 +29,7 @@ git clone https://github.com/fw876/helloworld.git package/helloworld
 # git clone https://github.com/jlzsss/luci-app-ssr-plus-jo-depend.git package/luci-app-ssr-plus-jo-depend
 svn co https://github.com/openwrt/packages/trunk/libs/libcap package/feeds/packages/libcap
 svn co https://github.com/Lienol/openwrt/trunk/package/diy package/diy
-svn co https://github.com/Lienol/openwrt-packages/tree/main/lang/php7 package/feeds/packages/php7
+svn co https://github.com/Lienol/openwrt-packages/trunk/lang/php7 package/feeds/packages/php7
 rm -rf package/diy/luci-app-dockerman
 git clone https://github.com/jlzsss/docker-ce.git package/docker-ce
 # git clone https://gitee.com/wangfuying_admin/Docker-For-OpenWRT.git package/docker
