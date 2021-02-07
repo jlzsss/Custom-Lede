a794e399d62adeb8c805215bee2c86a58c10c8a1
jlzsss <jlz19821031@gmail.com>
2021-02-07 20:25:43 +0800

    Update diy-part2-lede.sh

--- diy-part2-lede.sh
+++ diy-part2-lede.sh
@@ -11,8 +11,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 # git clone https://github.com/jlzsss/luci-app-passwall.git package/luci-app-passwall
 # git clone https://github.com/jlzsss/openwrt-ssr-libev-full.git package/openwrt-ssr-libev-full
 # git clone https://github.com/jlzsss/openwrt-ssr.git package/openwrt-ssr
