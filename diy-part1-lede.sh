56ef65183616ab8606ae5dcd736865913be12845
jlzsss <jlz19821031@gmail.com>
2021-03-05 17:09:22 +0800

    Update diy-part1-lede.sh

--- diy-part1-lede.sh
+++ diy-part1-lede.sh
@@ -47,6 +47,4 @@ git clone https://github.com/NateLol/luci-app-oled.git package/luci-app-oled
 git clone https://github.com/jlzsss/dnscrypt-proxy2.git package/feeds/packages/dnscrypt-proxy2
 git clone https://github.com/jlzsss/smartdns.git package/feeds/packages/smartdns
 git clone https://github.com/jlzsss/luci-app-smartdns.git package/feeds/luci/luci-app-smartdns
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 # svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-transmission package/feeds/luci/luci-app-transmission
