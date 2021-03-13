132149c862381e94c48f6acc7b18de29e7ba9688
jlzsss <jlz19821031@gmail.com>
2021-03-13 17:58:09 +0800

    Update diy-part1-lede.sh

--- diy-part1-lede.sh
+++ diy-part1-lede.sh
@@ -30,7 +30,7 @@ git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luc
 # git clone https://github.com/jlzsss/luci-app-ssr-plus-jo-depend.git package/luci-app-ssr-plus-jo-depend
 svn co https://github.com/openwrt/packages/trunk/libs/libcap package/feeds/packages/libcap
 svn co https://github.com/Lienol/openwrt/trunk/package/diy package/diy
-rm -rf package/diy/luci-app-dockerman
+svn co https://github.com/Lienol/openwrt-packages/trunk/utils/dockerd package/utils/dockerd
 rm -rf package/diy/OpenAppFilter
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
